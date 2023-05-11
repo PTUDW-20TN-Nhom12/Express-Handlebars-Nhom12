61eed385fe385d0a848d4b41c5822cc9ca749f9f
mekanican <boommerang73@gmail.com>
2023-05-11 11:08:16 +0700

    add task1 emotion quote -20120536

--- routes/task1.js
+++ routes/task1.js
@@ -5,8 +5,18 @@ const {emotions} = require("../data");
 
 router.get("/", (req, res) => {
     res.render("task1", {
-        emotions: emotions
+        emotions: emotions,
+        quotePath: '/task1/default.jpg'
     })
 });
+
+router.get("/:emotion", (req, res) => {
+    let x = req.params.emotion;
+    x = `task1/${x}/${x}1.jpg`;
+    res.render("task1", {
+        emotions: emotions,
+        quotePath: x
+    })
+})
 
 module.exports = router;
