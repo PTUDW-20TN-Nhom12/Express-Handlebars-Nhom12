747b926880dbad04b17d4a807e5e4bebcc7b2247
Nguyen Pham Nhat Huy <84710394+CTIHUY@users.noreply.github.com>
2023-05-11 10:53:38 +0700

    update index.js

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-const  { 
-  emotions,
-  categories, 
-  products ,
-  zodiacs 
-} = require('./data.js');
-
-const fs = require('fs');
-
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
 const express_handlebars = require('express-handlebars');
 
 app.engine('.hbs', express_handlebars.engine({
@@ -16,31 +8,24 @@ app.engine('.hbs', express_handlebars.engine({
     extname: '.hbs',
     defaultLayout: 'layout'
 }));
-app.set('view engine', '.hbs');
-app.set('views', './views');
 
+app.set('view engine', '.hbs');
+
+app.set('views', './views');
 
 app.use(express.static(__dirname + "/public"));
 
-app.listen(9000, (err) => {
-    if(err) console.log(err);
-    console.log('listening on 9000');
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
 });
 
 app.get("/", (req, res) => {
     res.render("index");
 })
 
-app.get("/task1", (req, res) => {
-    
-});
-app.get("/task2", (req, res) => {
-
-});
-app.get("/task3", (req, res) => {
-
-});
-app.get("/task4", (req, res) => {
-
-});
+app.use('/task1', require('./routes/task1'));
+app.use('/task2', require('./routes/task2'));
+app.use('/task3', require('./routes/task3'));
+app.use('/task4', require('./routes/task4'));
+app.use('/task4', require('./routes/task4'));
 
